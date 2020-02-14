38c8682506c4ff8e1e0349e6ed9b5b7661fa5821
Chris <cnsheafe@pm.me>
2020-02-13 19:08:54 -0700

    autostart blueman, thunar, pasystray, and xbindkeys

--- .config/awesome/autorun.sh
+++ .config/awesome/autorun.sh
@@ -11,9 +11,20 @@ function run {
 # Make sure bluetooth is ready - especially for headsets.
 run bluetoothctl power on
 run bluetoothctl default-agent
+# Starts the Applet so that it can appear in the wibar
+run blueman-applet
 
 # Start Pulse Audio
 run systemctl --user start pulseaudio.service
 
 # Enable Albert - The App Launcher
 run albert
+
+# Enable Thunar - The File Manager
+run thunar --daemon
+
+# Activate Pulseaudio System Tray
+run pasystray
+
+# Bind Special Keys(e.g. Volume Control)
+run xbindkeys
