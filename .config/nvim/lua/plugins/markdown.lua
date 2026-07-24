dcdfa148348688fd65b3d3fbc53c5562d274f6ee
chris <cnsheafe@pm.me>
2026-07-24 14:28:24 -0600

    assign random available port for md live preview

--- .config/nvim/lua/plugins/markdown.lua
+++ .config/nvim/lua/plugins/markdown.lua
@@ -4,6 +4,30 @@ return {
   {
     "brianhuster/live-preview.nvim",
     cmd = "LivePreview",
+    config = function()
+      local min_port = 5500
+      local max_port = 65535
+      local port_count = max_port - min_port + 1
+      local first_offset = vim.uv.hrtime() % port_count
+
+      for offset = 0, port_count - 1 do
+        local port = min_port + ((first_offset + offset) % port_count)
+        local socket = vim.uv.new_tcp()
+
+        if socket then
+          local bound = socket:bind("127.0.0.1", port)
+          local listening = bound and socket:listen(1, function() end)
+          socket:close()
+
+          if listening then
+            require("livepreview.config").set({ port = port })
+            return
+          end
+        end
+      end
+
+      error(("live-preview.nvim: no available port between %d and %d"):format(min_port, max_port))
+    end,
     keys = {
       {
         "<leader>cp",
